5b34ff52607f46f3fe59ef1310a3c993391efe7e
RTopolowski <robert.topolowski@gmail.com>
2022-10-19 14:01:21 +0100

    Complete form tasks

--- things/views.py
+++ things/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from things.models import Thing
+from .forms import ThingForm
+
 def home(request):
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
